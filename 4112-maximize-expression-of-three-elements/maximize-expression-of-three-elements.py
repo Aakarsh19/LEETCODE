5a9f0873fe0c64b089c0895b76f16ef6c29e88d6
Aakarsh19 <aakarshsharma333@gmail.com>
2025-11-21 20:07:54 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (98.38%) - LeetSync

---
+++ 4112-maximize-expression-of-three-elements/maximize-expression-of-three-elements.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maximizeExpressionOfThree(self, nums: List[int]) -> int:
+        nums.sort()
+        a = nums[-1]
+        b = nums[-2]
+        c = nums[0]
+        return (a+b-c)
